d67ffe05ad722a10472b49b6bc10f47dca9bca61
alexstaritsky <alexstaritsky@hotmail.nl>
2016-05-12 13:02:39 +0200

    Class sequentie
    Class sequentie is gemaakt

--- BLAST-Script.py
+++ BLAST-Script.py
@@ -1,1 +1,37 @@
+"""
 
+ Titel: Auto BLAST
+ Beschijving: Automatisch BLASTen
+ Bronnen: Geen
+ Auteur: William Sies en Alex Staritsky
+ Datum: Thu May 12 12:36:07 2016
+ Versie: 1.0
+ Updates: Zie GitHub
+ © Copyright
+ 
+"""
+
+class sequentie:
+    def __init__(self, sequentie_id, kwaliteitsscore, sequentie, type_seq):
+        self._sequentie_id = sequentie_id
+        self._kwaliteits_score = kwaliteitsscore
+        self._sequentie = sequentie
+        self._type = type_seq
+        
+    def getValue(self, value):
+        if value == 'sequentie_id':
+            return self._sequentie_id
+        elif value == 'kwaliteitsscore':
+            return self._kwaliteits_score
+        elif value == 'sequentie':
+            return self._sequentie
+        elif value == 'type':
+            return self._type
+        else:
+            raise Exception('Geen of een verkeerde value opgevraagd!')
+        
+def main():
+    seq = sequentie(1, 'test', 'atgc', 'fwd')
+    print(seq.getValue('sequentie_id'))
+    
+main()
