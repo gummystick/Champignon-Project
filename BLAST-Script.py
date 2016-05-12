c57852a34d66282388c26f2e6e75254f1844bbe1
alexstaritsky <alexstaritsky@hotmail.nl>
2016-05-12 14:26:51 +0200

    Data Class
    Leest nu twee bestanden en maakt er een data bestand van.

--- BLAST-Script.py
+++ BLAST-Script.py
@@ -5,8 +5,8 @@
  Bronnen: Geen
  Auteur: William Sies en Alex Staritsky
  Datum: Thu May 12 12:36:07 2016
- Versie: 1.0
- Updates: Zie GitHub
+ Versie: 1.2
+ Updates: Geen
  © Copyright
  
 """
@@ -28,10 +28,42 @@ class sequentie:
         elif value == 'type':
             return self._type
         else:
-            raise Exception('Geen of een verkeerde value opgevraagd!')
+            raise ValueError('Geen of een verkeerde value opgevraagd!')
+    
+    def summary(self):
+        print('Sequentie_ID =', self._sequentie_id)
+        print('Kwaliteitsscore =', self._kwaliteits_score)
+        print('Sequentie =', self._sequentie)
+        print('Type =', self._type)
+
+class data:
+    def __init__(self, fwd_bestandnaam, rev_bestandnaam):
+        self._data = []
+        self.__readFile__(fwd_bestandnaam)
+        self.__readFile__(rev_bestandnaam)
+        
+    def __readFile__(self, bestandnaam):
+        with open(bestandnaam, 'r') as bestand:
+            regelnummer = -1
+            for regel in bestand:
+                regelnummer += 1
+                if regelnummer > 3:
+                    regelnummer = 0
+                if regelnummer == 0:
+                    sequentie_id = regel[1:regel.find('/')]
+                    type_seq = regel[-2]
+                if regelnummer == 1:
+                    seq = regel
+                if regelnummer == 3:
+                    kwaliteitsscore = regel
+                    self._data.append(sequentie(sequentie_id, kwaliteitsscore, seq, type_seq))
+            bestand.close()
+            
+    def getData(self):
+        return self._data
         
 def main():
-    seq = sequentie(1, 'test', 'atgc', 'fwd')
-    print(seq.getValue('sequentie_id'))
+    champignon_data = data('@HWI-M02942_file1.txt', '@HWI-M02942_file2.txt')
+    champignon_data.getData()[0].summary()
     
 main()
