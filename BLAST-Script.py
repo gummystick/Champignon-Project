8716e9e4fbfbf73ed477e2cf268693c645dd773f
alexstaritsky <alexstaritsky@hotmail.nl>
2016-05-23 18:32:47 +0200

    2.7 Server versie
    Draait nu op dit moment

--- BLAST-Script.py
+++ BLAST-Script.py
@@ -5,7 +5,7 @@
  Bronnen: Geen
  Auteurs: William Sies en Alex Staritsky
  Datum: Thu May 12 12:36:07 2016
- Versie: 2.5
+ Versie: 3.0
  Updates: Zie github
  (c) Copyright
  
@@ -23,8 +23,8 @@ class settings:
         try:
             with open(self.bestandnaam, 'r') as bestand:
                 for regel in bestand:
-                    if regel[0] != '#' and regel.replace('\n','').replace('\t','').replace(' ','') != '':
-                        self.settings.append(regel[regel.find(': ')+1:].replace('\n','').replace('\t','').replace(' ','')) 
+                    if regel[0] != '#' and regel.replace('\n','').replace('\t','').replace('\r\n','').replace('\n\r','').replace('\r','').replace(' ','') != '':
+                        self.settings.append(regel[regel.find(': ')+1:].replace('\n','').replace('\t','').replace('\r\n','').replace('\n\r','').replace('\r','').replace(' ','')) 
         except IOError or FileNotFoundError or UnicodeDecodeError:
             raise Exception('Settingsbestand niet gevonden, of kan het bestand niet lezen!')
 
@@ -87,16 +87,18 @@ class sequentie:
             raise ValueError('Geen of een verkeerde value opgevraagd!')
 
 class seq_data:
-    def __init__(self, fwd_bestandnaam, rev_bestandnaam):
+    def __init__(self, fwd_bestandnaam, rev_bestandnaam, parameters):
         self.data = []
         self.index = 0
-        self.__readFile__(fwd_bestandnaam)
-        self.__readFile__(rev_bestandnaam)
+        self.fwd_data = self.__readFile__(fwd_bestandnaam)
+        self.rev_data = self.__readFile__(rev_bestandnaam)
+        self.__filterData__(self.fwd_data, parameters)
+        self.__filterData__(self.rev_data, parameters)
         
     def __iter__(self):
         return self
     
-    def __next__(self):
+    def next(self):
         if self.index == len(self.data):
             raise StopIteration
         index = self.index
@@ -106,6 +108,7 @@ class seq_data:
     def __readFile__(self, bestandnaam):
         try:
             with open(bestandnaam, 'r') as bestand:
+                data = []
                 regelnummer = -1
                 for regel in bestand:
                     regelnummer += 1
@@ -113,16 +116,27 @@ class seq_data:
                         regelnummer = 0
                     if regelnummer == 0:
                         sequentie_id = regel[1:regel.find('/')]
-                        type_seq = regel[-2]
+                        type_seq = regel.replace('\n', '').replace('\r', '').replace(' ','')[-1]
                     if regelnummer == 1:
                         seq = regel
                     if regelnummer == 3:
                         kwaliteitsscore = regel
-                        self.data.append(sequentie(sequentie_id, kwaliteitsscore, seq, type_seq))
+                        data.append(sequentie(sequentie_id, kwaliteitsscore, seq, type_seq))
                 bestand.close()
+                return data
         except IOError or FileNotFoundError or UnicodeDecodeError:
             raise Exception('Kan sequentiebestand niet vinden of lezen!')
-            
+    
+    def __filterData__(self, data, parameters):
+        startReading = False
+        for sequentie in self.fwd_data:
+            if sequentie.getValue('sequentie_id') == parameters.get(26):
+                startReading = True
+            if startReading:
+                self.data.append(sequentie)
+            if sequentie.getValue('sequentie_id') == parameters.get(27):       
+                startReading = False
+    
     def set_loop(self, proces_number):
         self.index = int(proces_number)
     
@@ -131,7 +145,7 @@ class seq_data:
 
 class type_operation:
     def __init__(self):
-        self.operations = ['save_data', 'blastn', 'blastx', 'tblastx']
+        self.operations = ['save_sequentie', 'blastn', 'blastx', 'tblastx']
         self.index = 0
         self.resume = False
         
@@ -140,7 +154,7 @@ class type_operation:
             self.index = 0
         return self
         
-    def __next__(self):
+    def next(self):
         if self.index == len(self.operations):
             if self.resume:
                 self.resume = False
@@ -188,45 +202,47 @@ class BLAST:
 
 class database:
     def __init__(self, parameters):
-        # Maak verbinding met de database
+        # Variabelen voor verbinding
         self.connection = mysql.connector.connect(host=parameters.get(7), user=parameters.get(8), db=parameters.get(9), password=parameters.get(10))
         self.cursor = self.connection.cursor()
         
     def send_seq_data(self, sequentie):
         # Check of deze sequentie al in de database staat
-        self.cursor.execute("""SELECT s.Sequentie_identifier
-                               FROM Sequentie s, Sequentie_informatie i
-                               WHERE s.Sequentie_identifier = '{0}' AND i.Sequentie_identifier = '{0}' AND i.Type = {1}""").format(sequentie.getValue('sequentie_id'), sequentie.getValue('type'))
+        self.cursor.execute("""SELECT s.Sequentie_identifier 
+                               FROM Sequentie s, Sequentie_informatie i 
+                               WHERE s.Sequentie_identifier = '{0}' AND i.Sequentie_identifier = '{0}' AND i.Type = {1}""".format(sequentie.getValue('sequentie_id'), sequentie.getValue('type')))
         results = self.cursor.fetchall()
-        if results == []:
+        self.cursor.close()
+        try:
+            results[0]
+        except IndexError:
             # Als het er niet in staat, voeg het toe
+            self.cursor = self.connection.cursor()
             self.cursor.execute("INSERT INTO Sequentie VALUES ('{0}');".format(sequentie.getValue('sequentie_id')))
             self.cursor.execute("INSERT INTO Sequentie_informatie VALUES ('{0}', '{1}', {2}, '{3}');".format(sequentie.getValue('kwaliteitsscore'), sequentie.getValue('sequentie'), sequentie.getValue('type'), sequentie.getValue('sequentie_id')))
             self.connection.commit()
-            
+            self.cursor.close()
         # Sluit de verbinding
-        self.cursor.close()
         self.connection.close()
         
-    def send_blast_results(self, type_blast, sequentie, parameters, blast_record):
+    def send_blast_results(self, type_blast, sequentie, parameters, blast_record, program_log):
         # Create_id's
         self.cursor.execute("SELECT max(Parameters_ID) FROM Resultaat")
         results = self.cursor.fetchall()
-        if results == []:
-            parameters_id = 0
+        self.cursor.close()
+        if results[0][0] == None:
+            parameters_id = 1   
         else:
-            parameters_id = int(results[0])+1
+            parameters_id = int(results[0][0])+1
         query_id = str(parameters_id)
         resultaat_id = parameters_id
         
         # Has_results
-        result_test = ''
-        for alignment in results.alignments:
-            result_test += alignment
-        if result_test == '':
-            has_results = 0
-        else:
-            has_results = 1
+        has_results = 1
+        for alignment in blast_record.alignments:
+            for hsp in alignment.hsps:
+                if hsp.num_alignments == 'None':
+                    has_results = 0
         
         # Low_complexity en mask_lookup
         if (type_blast == 'blastn' and parameters.get(13) == 'True') or (type_blast == 'blastx' and parameters.get(18) == 'True') or (type_blast == 'tblastx' and parameters.get(24) == 'True'):
@@ -239,26 +255,50 @@ class database:
             mask_lookup = 0
             
         # Send parameters
+        self.cursor = self.connection.cursor()
         if type_blast == 'blastn':
-            self.cursor.execute("""INSERT INTO Parameters VALUES ('{0}', '{1}', NULL, NULL, {2}, NULL, {3}, NULL, NULL, {4}, {5});""".format(type_blast, parameters.get(11), low_complexity, mask_lookup, parameters_id, resultaat_id))
+            self.cursor.execute("""INSERT INTO Parameters VALUES ('{0}', '{1}', NULL, {2}, {3}, NULL, {4}, {5});""".format(type_blast, parameters.get(11), low_complexity, mask_lookup, parameters_id, resultaat_id))
         if type_blast == 'blastx':
-            self.cursor.execute("""INSERT INTO Parameters VALUES ('{0}', '{1}', NULL, {2}, {3}, NULL, {4}, NULL, {5}, {6}, {7});""".format(type_blast, parameters.get(15), parameters.get(16), low_complexity, mask_lookup, parameters.get(20), parameters_id, resultaat_id))
+            self.cursor.execute("""INSERT INTO Parameters VALUES ('{0}', '{1}', '{2}', {3}, {4}, '{5}', {6}, {7});""".format(type_blast, parameters.get(15), parameters.get(16), low_complexity, mask_lookup, parameters.get(20), parameters_id, resultaat_id))
         if type_blast == 'tblastx':
-            self.cursor.execute("""INSERT INTO Parameters VALUES ('{0}', '{1}', NULL, {2}, {3}, NULL, {4}, NULL, NULL, {5}, {6});""".format(type_blast, parameters.get(21), parameters.get(22), low_complexity, mask_lookup, parameters_id, resultaat_id))
+            self.cursor.execute("""INSERT INTO Parameters VALUES ('{0}', '{1}', '{2}', {3}, {4}, NULL, {5}, {6});""".format(type_blast, parameters.get(21), parameters.get(22), low_complexity, mask_lookup, parameters_id, resultaat_id))
         
         # Send BLAST
-        self.cursor.execute("""INSERT INTO Resultaat VALUES ('{0}', CURRENT_DATE(), CURRENT_TIME(), NULL, '{1}', {2}, {3}, {4});""".format(query_id, sequentie.getValue('sequentie_id'), resultaat_id , parameters_id, has_results))
+        self.cursor.execute("""INSERT INTO Resultaat VALUES ('{0}', CURRENT_DATE(), CURRENT_TIME(), '{1}', {2}, {3}, {4});""".format(query_id, sequentie.getValue('sequentie_id'), resultaat_id , parameters_id, has_results))        
         
         # Send eiwitten
-        for alignment in blast_record:
+        for alignment in blast_record.alignments:
             for hsp in alignment.hsps:
                 if hsp.expect < float(parameters.get(3)):
+                    total_aligned = hsp.align_length
+                    identity_perc = float(float(hsp.identities)/float(total_aligned))*100
+                    positives_perc = float(float(hsp.positives)/float(total_aligned))*100
+                    try:
+                        organisme_eiwit_info = alignment.hit_def.encode('utf-8')
+                    except AttributeError:
+                        organisme_eiwit_info = 'NULL'
+                        print("Unicode object bij sequentie '{0}': 'organisme_eiwit_info'".format(sequentie.getValue('sequentie_id')))
+                        program_log.write("Unicode object bij sequentie '{0}: 'organisme_eiwit_info'".format(sequentie.getValue('sequentie_id')))
+                    try:
+                        organisme_eiwit_ID = alignment.hit_id.encode('utf-8')
+                    except AttributeError:
+                        organisme_eiwit_ID = 'NULL'
+                        print("Unicode object bij sequentie '{0}': 'organisme_eiwit_ID'".format(sequentie.getValue('sequentie_id')))
+                        program_log.write("Unicode object bij sequentie '{0}: 'eiwit_ID'".format(sequentie.getValue('sequentie_id')))
+                    try:
+                        organisme_eiwit_title = alignment.title.encode('utf-8')
+                        print("Unicode object bij sequentie '{0}': 'organisme_eiwit_title'".format(sequentie.getValue('sequentie_id')))
+                        program_log.write("Unicode object bij sequentie '{0}: 'organisme_eiwit_title'".format(sequentie.getValue('sequentie_id')))
+                    except AttributeError:
+                        organisme_eiwit_title = 'NULL'
+                    alignment = hsp.query+'\n'+hsp.match+'\n'+hsp.sbjct
                     if type_blast == 'blastn':
-                        self.cursor.execute("""INSERT INTO BLAST_resultaat__informatie VALUES ('{0}', {1}, {2}, '{3}', '{4}', NULL, NULL, NULL, NULL, NULL, '{5}', '{6}', '{7}', {8});""".format(hsp.expect, hsp.bits, hsp.score, hsp.identities, hsp.gaps, hsp.query+' ; '+hsp.match+' ; '+hsp.sbjct, alignment.title.split('|')[4][:alignment.title.split('|')[4].find('RecName:')].replace(' ','')[5:], alignment.title[alignment.title.find('RecName:'):] , resultaat_id))
+                        self.cursor.execute("""INSERT INTO BLAST_resultaat__informatie VALUES ('{0}', {1}, {2}, {3}, {4}, {5}, {6}, {7}, {8}, NULL, '{9}', '{10}', '{11}', '{12}', {13});""".format(hsp.expect, hsp.bits, hsp.score, hsp.identities, hsp.positives, hsp.gaps, identity_perc, positives_perc, total_aligned, organisme_eiwit_ID, organisme_eiwit_title, organisme_eiwit_info, alignment, resultaat_id))
                     if type_blast == 'blastx':
-                        self.cursor.execute("""INSERT INTO BLAST_resultaat__informatie VALUES ('{0}', {1}, {2}, '{3}', '{4}', NULL, NULL, NULL, NULL, '{5}', '{6}', '{7}', '{8}', {9});""".format(hsp.expect, hsp.bits, hsp.score, hsp.identities, hsp.gaps, hsp.frame, hsp.query+' ; '+hsp.match+' ; '+hsp.sbjct, alignment.title.split('|')[4][:alignment.title.split('|')[4].find('RecName:')].replace(' ','')[5:], alignment.title[alignment.title.find('RecName:'):] , resultaat_id))
+                        self.cursor.execute("""INSERT INTO BLAST_resultaat__informatie VALUES ('{0}', {1}, {2}, {3}, {4}, {5}, {6}, {7}, {8}, '{9}', '{10}', '{11}', '{12}', '{13}', {14});""".format(hsp.expect, hsp.bits, hsp.score, hsp.identities, hsp.positives, hsp.gaps, identity_perc, positives_perc, total_aligned, hsp.frame, organisme_eiwit_ID, organisme_eiwit_title, organisme_eiwit_info, alignment, resultaat_id))
                     if type_blast == 'tblastx':
-                        self.cursor.execute("""INSERT INTO BLAST_resultaat__informatie VALUES ('{0}', {1}, {2}, '{3}', '{4}', NULL, NULL, NULL, NULL, '{5}', '{6}', '{7}', '{8}', {9});""".format(hsp.expect, hsp.bits, hsp.score, hsp.identities, hsp.gaps, hsp.frame, hsp.query+' ; '+hsp.match+' ; '+hsp.sbjct, alignment.title.split('|')[4][:alignment.title.split('|')[4].find('RecName:')].replace(' ','')[5:], alignment.title[alignment.title.find('RecName:'):] , resultaat_id))
+                        self.cursor.execute("""INSERT INTO BLAST_resultaat__informatie VALUES ('{0}', {1}, {2}, {3}, {4}, {5}, {6}, {7}, {8}, '{9}', '{10}', '{11}', '{12}', '{13}', {14});""".format(hsp.expect, hsp.bits, hsp.score, hsp.identities, hsp.positives, hsp.gaps, identity_perc, positives_perc, total_aligned, hsp.frame, organisme_eiwit_ID, organisme_eiwit_title, organisme_eiwit_info, alignment, resultaat_id))
+        
         # Commit data en close verbinding
         self.connection.commit()
         self.cursor.close()
@@ -267,7 +307,7 @@ class database:
 def main():
     # Init objecten
     parameters = settings('Auto_BLAST_settings.settings')
-    champignon_data = seq_data(parameters.get(0), parameters.get(1))
+    champignon_data = seq_data(parameters.get(0), parameters.get(1), parameters)
     operations = type_operation()
     program_log = log(parameters.get(2))
     
@@ -289,11 +329,14 @@ def main():
                     NCBI = BLAST()
                     NCBI.do_blast(function, sequentie, parameters)
                     db = database(parameters)
-                    db.send_blast_results(function, sequentie, parameters, NCBI.get_results())
+                    db.send_blast_results(function, sequentie, parameters, NCBI.get_results(), program_log)
                 program_log.write("{0} van sequentie '{1}' is gelukt!".format(function, sequentie_id))
             except Exception as error:
                 print("Error bij sequentie '{0}': '{1}'".format(sequentie_id, error))
                 program_log.write("Error bij sequentie '{0}': '{1}'".format(sequentie_id, error))
-                exit()
+                champignon_data.set_loop(parameters.get(4))
+                operations.set_loop(parameters.get(5))
+                print('5 minuten wachten tot nieuwe poging...')
+                time.sleep(300)
 
 main()
